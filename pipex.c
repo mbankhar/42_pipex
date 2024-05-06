4926c50226e46a9a4e19001c44e349f9a0f4fb02
mbankhar <162722039+mbankhar@users.noreply.github.com>
2024-05-06 13:47:59 +0200

    fixed multiple

--- pipex.c
+++ pipex.c
@@ -6,61 +6,20 @@
 /*   By: mbankhar <mbankhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 14:12:58 by mbankhar          #+#    #+#             */
-/*   Updated: 2024/05/04 16:34:06 by mbankhar         ###   ########.fr       */
+/*   Updated: 2024/05/06 09:36:29 by mbankhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-#include <sys/types.h>
 
-void	get_args(int argc, char **argv)
-{
-	t_var	var;
-
-	if (argc == 5)
-	{
-		if (argv[2] == NULL || argv[3] == NULL || argv[1] == NULL
-			|| argv[4] == NULL)
-			exit(EXIT_FAILURE);
-		var.cmd_one = ft_split(argv[2], ' ');
-		var.cmd_two = ft_split(argv[3], ' ');
-		var.file_one = ft_strdup(argv[1]);
-		var.file_one = ft_strdup(argv[4]);
-		printf("%s\n", var.cmd_one[0]);
-		printf("%s\n", var.cmd_one[1]);
-		printf("%s\n", var.cmd_one[2]);
-		printf("%s\n", var.cmd_two[0]);
-		printf("%s\n", var.cmd_two[1]);
-		printf("%s\n", var.cmd_two[2]);
-		// printf("%s\n", var.file_one[0]);
-		// printf("%s\n", var.file_one[1]);
-	}
-	else
-	{
-		exit(EXIT_FAILURE);
-	}
-}
-
-void	ft_free(char **array)
-{
-	int	i;
-
-	i = 0;
-	while (array[i])
-	{
-		free(array[i]);
-		i++;
-	}
-	free(array);
-}
-
-char	*get_path(char **env, char *cmd)
+char	*get_path(char **env, char *cmd, t_var var)
 {
 	int		i;
-	char	*path;
+	char	path[8];
 	char	**realpath;
+	char	*string;
 
-	path[7] = "PATH=";
+	ft_strcpy(path, "PATH=");
 	i = 0;
 	while (env[i])
 	{
@@ -69,31 +28,27 @@ char	*get_path(char **env, char *cmd)
 		else
 			i++;
 	}
-	realpath = ft_split(env[i], ":");
-	i = 0;
-	while (realpath[i])
+	realpath = ft_split(env[i], ':');
+	i = -1;
+	while (realpath[++i])
 	{
-		if (access(ft_strjoin(realpath[i], cmd), X_OK) == 0)
+		string = ft_strjoinslesh(realpath[i], cmd);
+		if (access(string, X_OK) == 0)
 			break ;
-		else
-			i++;
+		free(string);
 	}
 	ft_free(realpath);
-	return (ft_strjoin(realpath[i], cmd));
+	if (i == '\0')
+	{
+		ft_putendl_fd("No command", 2);
+		exit(0);
+	}
+	return (string);
 }
 
-void	execute(char *arg, char **env, char *cmd)
-{
-	char	*path_cmd;
-
-	path_cmd = get_path(env, cmd);
-	execve(path_cmd, cmd[1], env);
-}
-
-void	child(char **argv, int *pipefd, char **env)
+void	child(char **argv, int *pipefd, char **env, t_var var)
 {
 	int		fd;
-	t_var	var;
 
 	fd = open(argv[1], O_RDONLY, 0777);
 	if (fd == -1)
@@ -101,13 +56,13 @@ void	child(char **argv, int *pipefd, char **env)
 	dup2(fd, 0);
 	dup2(pipefd[1], 1);
 	close(pipefd[0]);
-	execute(argv[2], env, var.cmd_one[1]);
+	execute(env, var.cmd_one[0], argv, var);
+	exit(0);
 }
 
-void	parent(char **argv, int *pipefd, char **env)
+void	parent(char **argv, int *pipefd, char **env, t_var var)
 {
 	int		fd;
-	t_var 	var;
 
 	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (fd == -1)
@@ -115,25 +70,35 @@ void	parent(char **argv, int *pipefd, char **env)
 	dup2(fd, 1);
 	dup2(pipefd[0], 0);
 	close(pipefd[1]);
-	execute(argv[3], env, var.cmd_two[1]);
+	executepar(env, var.cmd_two[0], argv, var);
+	exit(0);
 }
+
+// void	leaks(void)
+// {
+// 	system("leaks pipex");
+// }
 
 int	main(int argc, char **argv, char **env)
 {
 	int		pipefd[2];
 	pid_t	pid;
+	t_var	var;
 
-	env = NULL;
-	get_args(argc, argv);
+	// atexit(leaks);
+	get_args(argc, argv, &var);
 	if (pipe(pipefd) == -1)
 		exit(-1);
 	pid = fork();
 	if (pid == -1)
 		exit(-1);
 	if (pid == 0)
-		child(argv, pipefd, env);
-	parent(argv, pipefd, env);
+	{
+		child(argv, pipefd, env, var);
+	}
+	parent(argv, pipefd, env, var);
+	return (0);
 }
 
-
 // < test.txt grep a1 | wc -w > test2
+// ./pipex test.txt "grep a1" "wc -l" test2
---
+++ pipex_utils.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbankhar <mbankhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/04 17:13:37 by mbankhar          #+#    #+#             */
+/*   Updated: 2024/05/06 13:46:01 by mbankhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	get_args(int argc, char **argv, t_var *var)
+{
+	if (argc == 5)
+	{
+		if (argv[2] == NULL || argv[3] == NULL || argv[1] == NULL
+			|| argv[4] == NULL)
+			exit(EXIT_FAILURE);
+		if (ft_strchrmodif(argv[2]) != -1)
+			var->cmd_one = ft_splitmodif(argv[2], ' ');
+		else
+			var->cmd_one = ft_split(argv[2], ' ');
+		if (ft_strchrmodif(argv[3]) != -1)
+			var->cmd_two = ft_splitmodif(argv[3], ' ');
+		else
+			var->cmd_two = ft_split(argv[3], ' ');
+		// int	i = -1;
+		// while (var->cmd_one[++i])
+		// {
+		// 	printf("%s\n", var->cmd_one[i]);
+		// }
+		// i = -1;
+		// while (var->cmd_one[++i])
+		// {
+		// 	printf("%s\n", var->cmd_two[i]);
+		// }
+		// printf("%s\n", var->cmd_two[2]);
+		// printf("%s\n", var->cmd_two[3]);
+		// printf("%s\n", var->cmd_two[4]);
+		// exit(1);
+	}
+	else
+	{
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	ft_free(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
+
+void	execute(char **env, char *cmd, char **argv, t_var var)
+{
+	char	*path_cmd;
+
+	path_cmd = get_path(env, cmd, var);
+	if (execve(path_cmd, var.cmd_one, env) == -1)
+	{
+		ft_putstr_fd("pipex: command not found: ", 2);
+		ft_putendl_fd(var.cmd_one[0], 2);
+		ft_free(var.cmd_one);
+		exit(0);
+	}
+}
+
+
+void	executepar(char **env, char *cmd, char **argv, t_var var)
+{
+	char	*path_cmd;
+
+	path_cmd = get_path(env, cmd, var);
+	// system("leaks pipex");
+	if (execve(path_cmd, var.cmd_two, env) == -1)
+	{
+		ft_putstr_fd("pipex: command not found: ", 2);
+		ft_putendl_fd(var.cmd_two[0], 2);
+		ft_free(var.cmd_two);
+		exit(0);
+	}
+}
